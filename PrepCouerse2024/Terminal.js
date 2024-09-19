d5d91e715b34404345302e41e1cd4b7e29ab9a87
HRodriguezJimenez <harol469@gmail.com>
2024-09-19 10:42:33 -0500

    Harold2024

---
+++ PrepCouerse2024/Terminal.js
@@ -0,0 +1,42 @@
+/*//? Comando
+Es una intrucción que realiza una acción, ejecuta un proceso y produce una salida.
+
+//* Estructura de un comando
+- Nombre como descripción de la acción que realiza.
+- Recibe argumentos con los cuales le podemos especificar sobre que elemento queremos que se aplique el 
+comando.
+- Recibe opciones generalmente estan precedidas por un guión y alteran el comportamiento del comando.
+
+//? Algunos de los comandos mas usados:
+- pwd -> Print working directory = Imprimir el directorio.
+
+- ls -> List = listar: este comando muestra un listado con los nombres de los archivos y carpetas que se
+encuentran en la carpeta en la que estamos actualmente.
+
+- mkdir nombre -> Make directory = Crear carpeta: este comando crea una carpeta con el nombre que le demos.
+//! Si deseamos que el nombre de la carpeta tenga espacios entre sus palabras debemos escribir el nombre
+//! entre comillas "Nueva carpeta".
+
+- cd nombreDeLaCarpeta -> Change directory = Cambiar de carpeta o archivo.
+
+- cd .. -> Para regresar atras un directorio.
+
+- clear -> Para limpiar la terminal.
+
+- touch nombreDelArchivo.js -> Crear archivo vacio = Debemos siempre especificar la extención del archivo
+si es js, css, html, etc.
+
+- rm nombre -> Remove = Elimina el archivo seleccionado.
+
+- rm -r Nombre -> Elimina la carpeta que le indiquemos.
+
+- help -> Muestra en pantalla una lista de comandos básicos.
+
+
+
+
+
+
+
+
+*/
